409126d0c432ffc1dcff3a8c76d914ad8d588410
tristan <jametri014@gmail.com>
2022-07-28 13:29:11 -0400

    added cat class

---
+++ src/main/java/io/zipcoder/polymorphism/Cat.java
@@ -0,0 +1,18 @@
+package io.zipcoder.polymorphism;
+
+public class Cat extends Pet implements Comparable<Pet> {
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public void speak() {
+        System.out.println("Hiss!");
+    }
+}
